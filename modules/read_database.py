9cc748496b16e2da762f357f5440b30f93462df1
AntDavid <davidantoniodavi@gmail.com>
2024-07-26 04:08:33 -0300

    Updating directory navigation to load CSV files

--- modules/read_database.py
+++ modules/read_database.py
@@ -1,16 +1,24 @@
 import pandas as pd
+import os
 
 # Função para carregar dados
 def load_data(file_path):
-    return pd.read_csv(file_path)
+    
+    parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    
+    abs_path = os.path.join(parent_dir, 'data', file_path)
+    if not os.path.isfile(abs_path):
+        raise FileNotFoundError(f"O arquivo não foi encontrado: {abs_path}")
+    return pd.read_csv(abs_path)
+
 
 # Carregar dados
-def read_database(load_data):
-    situacao_escolar = load_data('../data/situacao_escolar.csv')
-    nivel_escolarizacao = load_data('../data/nivel_escolarizacao.csv')
-    idade_trabalho = load_data('../data/idade_trabalho.csv')
-    trabalho_cotas = load_data('../data/trabalho_cotas.csv')
-    municipios_instituicoes = load_data('../data/municipios_instituicoes.csv')
+def read_database():
+    situacao_escolar = load_data('situacao_escolar.csv')
+    nivel_escolarizacao = load_data('nivel_escolarizacao.csv')
+    idade_trabalho = load_data('idade_trabalho.csv')
+    trabalho_cotas = load_data('trabalho_cotas.csv')
+    municipios_instituicoes = load_data('municipios_instituicoes.csv')
     
     return {
         "situacao escolar": situacao_escolar,
