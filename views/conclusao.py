558370d2c5478ad34fdd3f6402f90bd36f35dc04
AntDavid <davidantoniodavi@gmail.com>
2024-07-28 19:55:45 -0300

    complementing the conclusion section.

--- views/conclusao.py
+++ views/conclusao.py
@@ -11,6 +11,15 @@ def conclusao():
             A baixa frequência escolar em algumas faixas etárias e a defasagem idade-série evidenciam a necessidade de políticas educacionais mais inclusivas e adaptadas às necessidades específicas das pessoas com TEA. O Atendimento Educacional Especializado (AEE) desempenha um papel crucial nesse contexto, fornecendo suporte individualizado que pode ajudar a aumentar a frequência e a permanência escolar desses alunos.
         <div>
         <div style='text-align: justify;'>                
-            No âmbito laboral, a maioria das pessoas com TEA não está empregada, apesar do potencial de inclusão no mercado de trabalho. A implementação de políticas públicas e iniciativas que promovam a inclusão dessas pessoas é fundamental para garantir seu direito ao trabalho e sua participação na sociedade.
+            Para melhorar a inclusão educacional, é fundamental o desenvolvimento e a implementação de Planos Educacionais Individualizados (PEI), adaptando o currículo às necessidades específicas de cada aluno com TEA. A expansão do AEE, com o aumento de salas de recursos multifuncionais e a formação de mais profissionais especializados, também é crucial. Além disso, a capacitação contínua de professores e funcionários é necessária para prepará-los a lidar com as necessidades dos alunos com TEA, promovendo um ambiente educacional mais acolhedor e inclusivo.
         </div>
+        <div style='text-align: justify;'>                
+           No âmbito laboral, a maioria das pessoas com TEA não está empregada, apesar do potencial de inclusão no mercado de trabalho. A implementação de políticas públicas e iniciativas que promovam a inclusão dessas pessoas é fundamental para garantir seu direito ao trabalho e sua participação na sociedade. Isso inclui a criação de programas de capacitação profissional específicos para pessoas com TEA, que levem em consideração suas habilidades e interesses, bem como a sensibilização de empregadores sobre a importância da inclusão e as vantagens de contratar pessoas com TEA. 
+        </div>  
+        <div style='text-align: justify;'>                
+            A promoção de parcerias entre escolas, famílias e empresas também é essencial. As famílias desempenham um papel crucial no apoio ao desenvolvimento educacional e profissional das pessoas com TEA, e a colaboração entre todos os envolvidos pode resultar em estratégias mais eficazes e personalizadas. As empresas, por sua vez, podem oferecer oportunidades de estágio e emprego, contribuindo para a integração dessas pessoas no mercado de trabalho.
+        </div>  
+        <div style='text-align: justify;'>                
+            Em resumo, a situação escolar e laboral das pessoas com TEA no Rio Grande do Sul exige uma abordagem multifacetada, que inclua a adaptação de políticas educacionais, a capacitação de profissionais da educação e do mercado de trabalho, e a promoção de parcerias colaborativas. Somente assim será possível superar os obstáculos existentes e garantir que as pessoas com TEA tenham acesso a uma educação de qualidade e a oportunidades de trabalho que promovam sua autonomia e participação plena na sociedade.
+        </div>     
     """, unsafe_allow_html=True)
